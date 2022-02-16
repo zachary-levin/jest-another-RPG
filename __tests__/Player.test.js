3aa157e1a1da4de65d1fc70c75c62aa1b6ad8a6c
zachary-levin <60529010+zachary-levin@users.noreply.github.com>
2022-02-16 00:54:19 -0600

    add mock for Potion and refactor player constructor

---
+++ __tests__/Player.test.js
@@ -0,0 +1,19 @@
+const Player = require('../lib/Player');
+const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion');
+
+console.log(new Potion());
+
+test('creates a player object', () => {
+    const player = new Player('Dave');
+
+    expect(player.name).toBe('Dave');
+    expect(player.health).toEqual(expect.any(Number));
+    expect(player.strength).toEqual(expect.any(Number));
+    expect(player.agility).toEqual(expect.any(Number));
+
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    );
+})
